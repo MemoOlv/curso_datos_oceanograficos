890382324eb88bde5d23c47f163f43e6ef7db995
MemoOlv <guillermo.olvera@islas.org.mx>
2023-07-25 17:51:24 -0600

    🧪 Failing test

--- tests/test_open_netcdf.py
+++ tests/test_open_netcdf.py
@@ -2,5 +2,5 @@ from ocean_data import read_netcdf_from_path
 
 
 def test_netcdf():
-    read_netcdf_from_path()
-    
+    netcdf_path = "tests/data/SST_20200111.nc"
+    read_netcdf_from_path(netcdf_path)
