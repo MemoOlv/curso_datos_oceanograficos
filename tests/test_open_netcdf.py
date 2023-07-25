0b93337e1c58748e1e2391e33fc976a4d5005fb4
MemoOlv <guillermo.olvera@islas.org.mx>
2023-07-25 17:58:41 -0600

    🧪 Failing test

--- tests/test_open_netcdf.py
+++ tests/test_open_netcdf.py
@@ -3,4 +3,8 @@ from ocean_data import read_netcdf_from_path
 
 def test_netcdf():
     netcdf_path = "tests/data/SST_20200111.nc"
-    read_netcdf_from_path(netcdf_path)
+    netcdf_data = read_netcdf_from_path(netcdf_path)
+    obtained_variables = list(netcdf_data.variables.keys())
+    expected_variables = ["time", "latitude", "longitude", "analysed_sst", "mask"]
+    assert obtained_variables == expected_variables
+    
