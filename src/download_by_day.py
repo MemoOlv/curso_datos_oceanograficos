63423959a4111e2b8cd4eab41570b1141f3c942c
MemoOlv <guillermo.olvera@islas.org.mx>
2023-07-25 17:32:53 -0600

    ✨ create directory

--- src/download_by_day.py
+++ src/download_by_day.py
@@ -1,7 +1,9 @@
 from ocean_data import download_from_day
+import os
 
 
 year = 2014
 directory = f"descargas/{year}/"
+os.makedirs(directory)
 for day_number in range(12):
     download_from_day(day_number + 1, year, directory)
