3d1380db17ecf279564048c88335373fc753abc5
MemoOlv <guillermo.olvera@islas.org.mx>
2023-07-25 19:27:18 -0600

    😱 The class go faster than tests

--- ocean_data/plot_temperature_lat_lon.py
+++ ocean_data/plot_temperature_lat_lon.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import glob
+from PIL import Image
 
 
 def plot_temperature_with_lat_lon(longitude, latitude, temperature_1, filename):
     plt.pcolor(longitude, latitude, temperature_1)
     plt.xlabel("Longitude (ºW)")
     plt.ylabel("Latitude (ºN)")
-    plt.grid()
-    plt.colorbar()
     plt.clim(25, 30)
     plt.savefig(filename)
 
@@ -17,3 +17,10 @@ def filter_temperature(temperature_1, temp_max, temp_min):
     filtered_temperature_1[temperature_1 > temp_max] = np.nan
     filtered_temperature_1[temperature_1 < temp_min] = np.nan
     return filtered_temperature_1
+
+
+def make_gif(frame_folder):
+    frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.png")]
+    frame_one = frames[0]
+    frame_one.save("my_awesome.gif", format="GIF", append_images=frames,
+               save_all=True, duration=100, loop=0)
--- src/read_mat_files.py
+++ src/read_mat_files.py
@@ -1,4 +1,4 @@
-from ocean_data import read_mat_files_from_path, plot_temperature_with_lat_lon, filter_temperature
+from ocean_data import read_mat_files_from_path, plot_temperature_with_lat_lon, filter_temperature, make_gif
 
 
 temperature_path = "tests/data/2015_sst_mountly.mat"
@@ -9,11 +9,16 @@ temperature = read_mat_files_from_path(temperature_path, "CompSST")
 longitude = read_mat_files_from_path(longitude_path, "varlon")[:, :]
 latitude = read_mat_files_from_path(latitude_path, "varlat")[:, :]
 
-temperature_1 = temperature[:, :, 0]
-
 temp_max = 33
 temp_min = 21
-filtered_temperature_1 = filter_temperature(temperature_1, temp_max, temp_min)
 
-file_name = "temperatura_1.png"
-plot_temperature_with_lat_lon(longitude, latitude, filtered_temperature_1, "temperatura_1.png")
+months = 12
+for i in range(months):
+    month_number = i
+    temperature_month = temperature[:, :, month_number]
+    filtered_temperature_month = filter_temperature(temperature_month, temp_max, temp_min)
+
+    filename = f"figures/temperatura_{month_number + 1}.png"
+    plot_temperature_with_lat_lon(longitude, latitude, filtered_temperature_month, filename)
+
+make_gif("figures")
