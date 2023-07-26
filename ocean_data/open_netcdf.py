dfdc7a9f9bacfff13f97108fb98e5ec6ca4114df
MemoOlv <guillermo.olvera@islas.org.mx>
2023-07-25 18:13:17 -0600

    ✅ Pass test

--- ocean_data/open_netcdf.py
+++ ocean_data/open_netcdf.py
@@ -1,8 +1,9 @@
 import netCDF4
+import scipy.io
 
 
 def read_netcdf_from_path(netcdf_path):
     return netCDF4.Dataset(netcdf_path)
 
 def read_mat_files_from_path(mat_path):
-    pass
+    return scipy.io.loadmat(mat_path)
