ef48c9668346b16aab1f5f495e6e4c832f02e59d
MemoOlv <guillermo.olvera@islas.org.mx>
2023-07-25 18:05:45 -0600

    ♻️ Refactor

--- tests/test_open_netcdf.py
+++ tests/test_open_netcdf.py
@@ -1,10 +1,9 @@
 from ocean_data import read_netcdf_from_path
 
 
-def test_netcdf():
+def test_read_netcdf_from_path():
     netcdf_path = "tests/data/SST_20200111.nc"
     netcdf_data = read_netcdf_from_path(netcdf_path)
     obtained_variables = list(netcdf_data.variables.keys())
     expected_variables = ["time", "latitude", "longitude", "analysed_sst", "mask"]
     assert obtained_variables == expected_variables
-    
