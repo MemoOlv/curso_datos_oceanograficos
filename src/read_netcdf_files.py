e14c301d5bb05b675127b34ebda5b3eb0a6a36be
MemoOlv <guillermo.olvera@islas.org.mx>
2023-07-27 17:26:18 -0600

    🚀 Implement in src

--- src/read_netcdf_files.py
+++ src/read_netcdf_files.py
@@ -1,4 +1,4 @@
-from ocean_data import read_netcdf_from_path, plot_variable_with_lat_lon, get_latitude_and_longitude
+from ocean_data import read_netcdf_from_path, plot_variable_with_lat_lon, get_latitude_and_longitude, extract_temporal_dimension
 from glob import glob
 
 netcdf_files = glob("tests/data/SST*.nc")
@@ -8,7 +8,8 @@ def plot_netcdf_files(netcdf_files, variable_name):
     for i in range(0, len(netcdf_files)):
         netcdf_data = read_netcdf_from_path(netcdf_files[i])
         longitude, latitude = get_latitude_and_longitude(netcdf_data)
-        variable = netcdf_data[variable_name][0]
+        temporal_dimension = extract_temporal_dimension(netcdf_data, variable_name)
+        variable = netcdf_data[variable_name][temporal_dimension]
 
         filename = f"{variable_name}_{i+1}.png"
         plot_variable_with_lat_lon(longitude, latitude, variable, filename)
