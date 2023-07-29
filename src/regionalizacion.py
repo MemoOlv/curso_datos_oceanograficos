be3aacbb64f40dd46878ebead9ac682e447cd559
MemoOlv <guillermo.olvera@islas.org.mx>
2023-07-28 18:27:27 -0600

    ✨ Compute eigenvalues and eigenvectors

--- src/regionalizacion.py
+++ src/regionalizacion.py
@@ -1,6 +1,5 @@
 from ocean_data import read_netcdf_from_path, AEE
 from glob import glob
-from functools import reduce
 import numpy as np
 
 
@@ -17,16 +16,23 @@ for i in range(0, len(netcdf_files)):
     del variable
 
     temperature = netcdf_data[variable_name][0]
-    temp_dim = reduce((lambda x, y: x * y), temperature.shape)
-    flatten_temperature = temperature.reshape(temp_dim)
+    flatten_temperature = temperature.T.reshape(-1)
     flatten_temperatures.append(flatten_temperature)
 
 
-M_sin_estandarizar = np.array(flatten_temperatures)
-print(M_sin_estandarizar)
+M_sin_estandarizar = flatten_temperatures
+print(np.nanmin(M_sin_estandarizar))
 
 M_estandarizada = [AEE(i) for i in M_sin_estandarizar ]
+print("Tamaño: ", np.shape(M_estandarizada))
 print("Prueba de estandarización (media): ", np.nanmean(M_estandarizada[0]))
 print("Prueba de estandarización (std): ", np.std(M_estandarizada[0]))
 
 
+
+M_cov = np.cov(M_estandarizada)
+print("Cov matriz:", M_cov)
+
+eigenvalues, eigenvectors = np.linalg.eig(M_cov)
+print("Eigenvalues:", eigenvalues)
+print("Eigenvectors:", eigenvectors)
